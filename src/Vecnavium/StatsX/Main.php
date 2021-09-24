505f7f05e082734e533e9faa6e50b35c2c3c65d4
Vecnavium <83182010+Vecnavium@users.noreply.github.com>
2021-09-24 06:34:44 -0400

    Updated namespaces

---
+++ src/Vecnavium/StatsX/Main.php
@@ -0,0 +1,109 @@
+<?php /** @noinspection PhpUnused */
+
+declare(strict_types=1);
+
+namespace Vecnavium\StatsX;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use Vecnavium\StatsX\Commands\StatsCommand;
+use Vecnavium\StatsX\Provider\UserDataSessionProvider;
+
+/**
+ * Class Main
+ * @package Vecnavium\StatsX
+ */
+class Main extends PluginBase implements Listener
+{
+
+	/** @var Main */
+	private static $instance;
+	/** @var UserDataSessionProvider[] */
+	private $sessions = [];
+
+
+	public function onEnable(): void
+	{
+		self::$instance = $this;
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getCommandMap()->register("StatsX", new StatsCommand($this));
+		
+	}
+
+
+	/**
+	 * @param Player $player
+	 * @return UserDataSessionProvider|null
+	 */
+	public function getSessionFor(Player $player): ?UserDataSessionProvider
+	{
+		return $this->sessions[$player->getName()] ?? null;
+	}
+
+	/**
+	 * @param PlayerJoinEvent $event
+	 * @priority NORMAL
+	 */
+	public function onPlayerJoin(PlayerJoinEvent $event): void
+	{
+		$player = $event->getPlayer();
+		$this->sessions[$player->getName()] = new UserDataSessionProvider($player);
+	}
+
+
+	/**
+	 * @param EntityDamageEvent $event
+	 * @priority NORMAL
+	 */
+	public function onEntityDamage(EntityDamageEvent $event): void
+	{
+		$victim = $event->getEntity();
+		if (!$victim instanceof Player) {
+			return;
+		}
+		if ($event instanceof EntityDamageByEntityEvent) {
+			$damager = $event->getDamager();
+			if (!$damager instanceof Player) {
+				return;
+			}
+			if ($event->getFinalDamage() > $victim->getHealth()) {
+				$damagerSession = $this->getSessionFor($damager);
+				$victimSession = $this->getSessionFor($victim);
+				$damagerSession->addKill();
+				$victimSession->addDeath($damager);
+			}
+			return;
+		}
+		if ($event->getFinalDamage() > $victim->getHealth()) {
+			$session = $this->getSessionFor($victim);
+			$session->addDeath();
+		}
+	}
+
+	/**
+	 * @param PlayerQuitEvent $event
+	 * @priority NORMAL
+	 */
+	public function onPlayerQuit(PlayerQuitEvent $event): void
+	{
+		$player = $event->getPlayer();
+		if (isset($this->sessions[$player->getName()])) unset($this->sessions[$player->getName()]);
+	}
+
+	/**
+	 * @return Main
+	 */
+	public static function getInstance(): Main
+	{
+		return self::$instance;
+	}
+
+
+}
+
